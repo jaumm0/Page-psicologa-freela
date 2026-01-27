85f731b79249d6b1f6da87b13c426314d7efa61a
Jaumm0 <jv224063@gmail.com>
2026-01-27 10:27:15 -0400

    arrumando erros

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,34 @@
 import { useState } from 'react'
 import './App.css'
-import Header from './components/header'
+import Header from './components/Header'
 import Main from './components/Main'
 import Info from './components/Info'
 import Services from './components/Services'
 import Feadback from './components/Feadback'
 import Contact from './components/Contact'
 import Footer from './components/Footer'
+import BookingModal from './components/BookingModal'
+import WhatsAppButton from './components/WhatsAppButton'
 
 function App() {
- 
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const openModal = () => setIsModalOpen(true);
+  const closeModal = () => setIsModalOpen(false);
 
   return (
     <>
     <section>
-    <Header/>
+    <Header onAgendarClick={openModal} />
     </section>
-    <Main/>
+    <Main onAgendarClick={openModal} />
     <Info/>
     <Services/>
     <Feadback/>
-    <Contact/>
+    <Contact onAgendarClick={openModal} />
     <Footer/>
+    <BookingModal isOpen={isModalOpen} onClose={closeModal} />
+    <WhatsAppButton />
     </>
   )
 }
